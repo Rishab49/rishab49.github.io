84ebc99fe3476d545bf63ee17abfddfaaa7f4452
Rishab49 <25582966+Rishab49@users.noreply.github.com>
2026-04-12 11:09:38 +0530

    adding blog4

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,12 +4,14 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				'heading': '"Fira Code", monospace',
-				'body': ' "PT Sans", sans-serif'
+				'heading': '"IBM Plex Mono", monospace',
+				'body': '"Geist", sans-serif'
 			},
 			colors: {
-				"primary": '#151515',
-				"secondary": '#323232',
+				"primary": '#0a0a0a',
+				"secondary": '#171717',
+				"textPrimary":"#ededed",
+				"textSecondary":"#a1a1aa"
 			},
 			fontSize:{
 				"1x":"100%",
@@ -20,6 +22,7 @@ export default {
 				"6x":"350%",
 				"1.3x":"130%"
 			},
+
 			backgroundImage:{
 				"noise-pattern":"url(/grain.svg),linear-gradient(to right, rgba(255, 0, 0, 0.2), rgba(0, 0, 255, 0.2)),linear-gradient(#151515,#151515)"
 			}
